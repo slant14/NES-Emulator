c8a71783581d7873507533f849524e38c7d5b320
Slan14 <mahmood.naderi98@gmail.com>
2025-08-05 12:04:21 +0300

    feat(Bus): add bus header file

---
+++ Bus.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include <cstdint>
+#include <array>
+
+class Bus {
+public:
+    Bus();
+    ~Bus() = default;
+public: 
+    cpu6502 cpu;
+private:
+    static constexpr uint16_t RAM_SIZE = 64 * 1024;
+    std::array<uint8_t, RAM_SIZE> ram;
+public:
+    void write(uint16_t address, uint8_t data);
+    uint8_t read(uint16_t address, readOnly = false) const;
+}
